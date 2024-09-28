bb47996562cbf4cca319a885ade20be7edd9cf67
ibnuhadjarmunthe <ibnuhadjar2003@gmail.com>
2024-09-28 12:23:51 +0700

    Add files via upload

---
+++ latihan-11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i <20; i++) {
+		for (int j = 0; j <10; j++) {
+		for (int k = 0; k <5; k++){
+		
+		cout<<"perulangan ke-"<< i <<","<< j <<","<<
+k <<endl;
+		}
+		}
+	}
+	return 0;
+}
