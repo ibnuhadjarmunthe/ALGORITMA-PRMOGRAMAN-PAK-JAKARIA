041366f92c81824ce72e386afe5636eea95fe9ee
ibnuhadjarmunthe <ibnuhadjar2003@gmail.com>
2024-09-28 12:37:26 +0700

    Add files via upload

---
+++ latihan-13.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int jumlahdata,jumlahbarang,totalpenjualan = 0;
+	
+	cout<<"masukkan jumlah data :";
+	cin>>jumlahdata;
+	
+	for (int i = 1; i <=jumlahdata; i++) {
+		cout<<"masukkan jumlah barang yang terjual di toko"<< i <<": ";
+		cin>>jumlahbarang;
+		
+		totalpenjualan += jumlahbarang; 
+	}
+	
+	cout<<"total barang yang terjual di toko adalah:"<<totalpenjualan<<endl;
+	
+	return 0;
+}
